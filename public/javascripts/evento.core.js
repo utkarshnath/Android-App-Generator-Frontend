cb7ad89c2a211ac10ebb1925dfde19d51ea80118
Sparsh Bansal <sparsh.bansal17895@gmail.com>
2017-04-24 12:24:34 +0530

    Renamed and refactored

--- public/javascripts/evento.core.js
+++ public/javascripts/evento.core.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
 	
+
+	/************************ GLIDE *****************************************************************8*/
+
+	// Initiate glide
+	$("#Glide").glide({
+	  type: "carousel"
+	});
+
+
+	/************************ SMOOTH SCROLL *****************************************************************8*/
+
+	// Callback when smooth scroll finishes , use it for animations and other stuff
 	callback = function(anchor,toggle){
 
 	};
 
+	// Initiate Smooth scroll
 	smoothScroll.init({
 		selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
 		selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
@@ -13,10 +26,17 @@ $(document).ready(function(){
 		callback: callback // Function to run after scrolling
 	});
 
-	// Add on click listener
+
+	/************************ EVENT LISTENERS *****************************************************************8*/
+
+
+	// Add on click listener for get started button
+	// Clicking that button smooth scrolls to event selection fragment of the page
 	$("#b_started").click(function(){
 			
 		console.log("Button Clicked!!");
 		smoothScroll.animateScroll(document.querySelector("#event_selection"));
 	});
+
 });
+
